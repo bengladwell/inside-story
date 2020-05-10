c1816f28cdde72f13fc1be49cd6f03fca464b5e8
Ben Gladwell <bgladwell@covermymeds.com>
2020-05-10 19:41:42 -0400

    WIP - get cognito working

--- src/components/header.js
+++ src/components/header.js
@@ -3,22 +3,36 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import './header.scss'
 
-const Header = ({ siteTitle }) => (
-  <header className='site-header'>
-    <div>
-      <Link
-        className='logo'
-        title={siteTitle}
-        to='/'
-        style={{
-          textDecoration: 'none'
-        }}
-      >
-        G
-      </Link>
-    </div>
-  </header>
-)
+import CognitoIdentity from 'aws-sdk/clients/cognitoidentity'
+
+const cognitoIdentity = new CognitoIdentity()
+
+const Header = ({ siteTitle }) => {
+  const login = async () => {
+    const credentials = await cognitoIdentity.getCredentialsForIdentity({
+      IdentityId: "us-east-1:7750fa7b-1fb4-41b3-8a6b-993cc405f7ef"
+    })
+    console.log(credentials)
+  }
+
+  return (
+    <header className='site-header'>
+      <div>
+        <Link
+          className='logo'
+          title={siteTitle}
+          to='/'
+          style={{
+            textDecoration: 'none'
+          }}
+        >
+          G
+        </Link>
+        <button className="site-header__login" onClick={login}>Login</button>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string
