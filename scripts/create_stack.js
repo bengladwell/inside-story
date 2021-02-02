d4e987cfdc0ef0761cdee685b1be10bac495127c
Ben Gladwell <bgladwell@covermymeds.com>
2021-02-02 14:36:31 -0500

    Create stack with JS script

---
+++ scripts/create_stack.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+const CloudFormation = require('aws-sdk/clients/cloudformation')
+const { promisify } = require('util')
+const exec = promisify(require('child_process').exec)
+const readFile = promisify(require('fs').readFile)
+
+// TODO: figure out what to do with .env files
+require('dotenv').config()
+
+const cloudFormation = new CloudFormation({ region: process.env.AWS_REGION })
+
+async function branchName () {
+  const { stdout, stderr } = await exec('git branch --show-current')
+  if (stderr) throw new Error(stderr)
+
+  return stdout.substring(0, stdout.length - 1)
+}
+
+async function createStack () {
+  try {
+    const cfTemplate = await readFile('lib/cloudformation.yml', 'utf8')
+    return await cloudFormation.createStack({
+      StackName: await branchName(),
+      TemplateBody: cfTemplate,
+      Capabilities: ['CAPABILITY_NAMED_IAM'],
+      Parameters: [{
+        ParameterKey: 'FacebookClientId',
+        ParameterValue: process.env.FACEBOOK_CLIENT_ID
+      }, {
+        ParameterKey: 'FacebookClientSecret',
+        ParameterValue: process.env.FACEBOOK_CLIENT_SECRET
+      }, {
+        ParameterKey: 'VideoAssetArn',
+        ParameterValue: process.env.VIDEO_ASSET_ARN
+      }]
+    }).promise()
+  } catch (err) {
+    console.log('Error creating stack')
+    throw err
+  }
+}
+
+(async () => {
+  try {
+    console.log(await createStack())
+  } catch (err) {
+    console.log(err)
+  }
+})()
