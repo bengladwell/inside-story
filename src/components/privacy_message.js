f9dddab94166b8b556ff388e449d0108999c6868
Ben Gladwell <bgladwell@covermymeds.com>
2021-04-02 08:33:02 -0400

    Add privacy page

---
+++ src/components/privacy_message.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './privacy_message.scss'
+
+const PrivacyMessage = () => {
+  return (
+    <div className="privacy-message">
+      <p>Hi there. When you log in to this site, I retain your name and email address.</p>
+      <p>If you would like me to delete your name and email address, I am happy to do so.</p>
+      <p>Just reach out to me in the same way that you did to request access.</p>
+    </div>
+  )
+}
+
+export default PrivacyMessage
---
+++ src/pages/privacy.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import PrivacyMessage from '../components/privacy_message'
+
+const PrivacyPage = () => {
+  return (
+    <Layout>
+      <PrivacyMessage />
+    </Layout>
+  )
+}
+
+export default PrivacyPage
