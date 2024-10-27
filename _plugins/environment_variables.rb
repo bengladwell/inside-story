c5e4637d85ecb9c96540a8e89d082e0be9efa85f
Ben Gladwell <bengladwell@gmail.com>
2024-10-27 19:53:29 -0400

    Setup Google login

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -1,4 +1,5 @@
 require 'dotenv'
+require 'uri'
 
 Dotenv.load
 
@@ -7,7 +8,8 @@ module Jekyll
   class EnvironmentVariablesGenerator < Generator
 
     def generate(site)
-      site.config['site_domain'] = ENV['SITE_DOMAIN']
+      site.config['site_domain'] = ENV.fetch('JEKYLL_ENV', 'development') == 'production' ? ENV['SITE_DOMAIN'] : 'local.bengladwell.com'
+      site.config['encoded_redirect_uri'] = URI.encode_www_form_component('https://' + site.config['site_domain'] + '/')
       site.config['video_domain'] = ENV['VIDEO_DOMAIN']
       site.config['signer_domain'] = ENV['SIGNER_DOMAIN']
       site.config['signer_path'] = ENV['SIGNER_PATH']
