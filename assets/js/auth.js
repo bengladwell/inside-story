8105d87ed686a929ead3642175e3193b85e6495a
Ben Gladwell <bengladwell@gmail.com>
2024-12-10 20:56:44 -0500

    Add withCredentials for video retrieval

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,36 +1,34 @@
 ---
 ---
-async function validateJWT() {
-  const token = Cookies.get('idToken')
-  if (!token) {
-    console.error('ID token not found in cookies.')
-    return;
-  }
-
+async function validateJWT(token) {
   const jwksUri = '{{ site.user_pool_provider_url }}/.well-known/jwks.json'
 
-  try {
-    const response = await fetch(jwksUri)
-    const jwks = await response.json()
+  const response = await fetch(jwksUri)
+  const jwks = await response.json()
 
-    const keyStore = jose.createRemoteJWKSet(new URL(jwksUri))
+  const keyStore = jose.createRemoteJWKSet(new URL(jwksUri))
 
-    const { payload } = await jose.jwtVerify(token, keyStore, {
-      issuer: '{{ site.user_pool_provider_url }}',
-    })
+  const { payload } = await jose.jwtVerify(token, keyStore, {
+    issuer: '{{ site.user_pool_provider_url }}',
+  })
 
-    return payload
+  return payload
 
-  } catch (err) {
-    console.error('JWT validation failed:', err);
-  }
 }
 
 (async () => {
-  const { name, picture } = await validateJWT();
-  const loginElement = document.querySelector('.login')
-  loginElement.style.display = 'none'
-  const userElement = document.querySelector('.user')
-  userElement.querySelector('img').src = picture
-  userElement.style.display = 'block'
+  const idToken = Cookies.get('idToken')
+
+  if (!idToken) return
+
+  try {
+    const { name, picture } = await validateJWT(idToken);
+    const loginElement = document.querySelector('.login')
+    loginElement.style.display = 'none'
+    const userElement = document.querySelector('.user')
+    userElement.querySelector('img').src = picture
+    userElement.style.display = 'block'
+  } catch (error) {
+    console.error(error)
+  }
 })()
--- scripts/update_cors.js
+++ scripts/update_cors.js
@@ -5,25 +5,30 @@ const { S3 } = require('@aws-sdk/client-s3');
 const s3 = new S3({ region: process.env.AWS_REGION })
 const Bucket = process.env.VIDEO_ASSET_BUCKET
 
-function hasRule (rules) {
+function hasRule (rules, domain) {
   return rules.CORSRules.find((rule) =>
-    rule.AllowedOrigins.includes(`https://${process.env.SITE_DOMAIN}`))
+    rule.AllowedOrigins.includes(`https://${domain}`))
 }
 
 (async () => {
   const corsRules = await s3.getBucketCors({ Bucket })
-  if (hasRule(corsRules)) {
-    console.log('CORS rule exists; skipping')
-  } else {
-    console.log(`Adding CORS rule for ${process.env.SITE_DOMAIN}`)
-    const newRules = {
-      CORSRules: corsRules.CORSRules.concat({
-        AllowedHeaders: ['*'],
-        AllowedMethods: ['HEAD', 'GET'],
-        AllowedOrigins: [`https://${process.env.SITE_DOMAIN}`],
-        ExposeHeaders: []
-      })
+
+  const newRules = [process.env.SITE_DOMAIN, 'local.bengladwell.com'].reduce((acc, domain) => {
+    if (hasRule(corsRules, domain)) {
+      console.log(`CORS rule exists for ${domain}; skipping`)
+      return acc
     }
-    await s3.putBucketCors({ Bucket, CORSConfiguration: newRules })
+
+    console.log(`Adding CORS rule for ${domain}`)
+    return [...acc, {
+      AllowedHeaders: ['*'],
+      AllowedMethods: ['HEAD', 'GET'],
+      AllowedOrigins: [`https://${domain}`],
+      ExposeHeaders: []
+    }]
+  }, [])
+
+  if (newRules.length) {
+    await s3.putBucketCors({ Bucket, CORSConfiguration: { CORSRules: [...corsRules.CORSRules, ...newRules] } })
   }
 })()
